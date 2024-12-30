d999a7e28af42f27b47235e454ab0cc3dd759716
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2024-12-30 10:40:35 +0700

    fix: minor update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -106,18 +106,6 @@ export const Hero = () => {
                 <ChevronRight size={24} />
             </button>
 
-            {/* Slide indicators */}
-            <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex space-x-2 z-20">
-                {slides.map((_, index) => (
-                    <button
-                        key={index}
-                        onClick={() => setCurrentSlide(index)}
-                        className={`w-3 h-3 rounded-full transition-colors ${
-                            currentSlide === index ? "bg-green-600" : "bg-white/50"
-                        }`}
-                    />
-                ))}
-            </div>
         </div>
     );
 };
