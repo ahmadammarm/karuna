5ce4fd4facacdae5e89ed74a458b467168376380
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2025-01-11 20:08:32 +0700

    feat: adding loading in ecoproduct page

--- src/app/(pages)/eco-product/page.tsx
+++ src/app/(pages)/eco-product/page.tsx
@@ -1,6 +1,22 @@
-import Banner from "@/components/layout/Banner";
+"use client"
 
-export default function EduProuctPage() {
+import Banner from "@/components/layout/Banner";
+import Loading from "@/components/layout/Loading";
+import { useState } from "react";
+
+export default function EcoProuctPage() {
+
+    const [isLoading, setIsLoading] = useState(true)
+
+    setTimeout(() => {
+        setIsLoading(false)
+    }, 2000)
+
+
+    if(isLoading) {
+        return <Loading />
+    }
+
     return (
         <div>
             <Banner title="Edu Product" imageUrl="/aset-home/banner-1.jpeg" href="/" link="Beranda" subtitle="Edu Product" />
